155a468fd3a6464300838efd1a660c53bd8ef2c0
davi-mbatista <machadobatista.davi@gmail.com>
2020-10-25 23:05:27 +0100

    Update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,10 @@ import { h } from 'preact';
 import { render, screen } from '@testing-library/preact';
 import App from './App';
 
-test('App renders', () => {
+beforeAll(() => {
     render(<App />);
+});
+
+test('App renders', () => {
     expect(screen.getByText(/app/i)).toBeInTheDocument();
 });
