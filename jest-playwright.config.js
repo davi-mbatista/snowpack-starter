cb59f23ac406873b9f5030073b49426e057841cd
Daví <davi@Davis-iMac.fritz.box>
2020-10-30 23:07:36 +0100

    Update jest-playwright.config.js

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -2,6 +2,7 @@ module.exports = {
     browsers: ['chromium'],
     serverOptions: {
         command: 'npm run serve',
-        port: 5000
+        port: 5000,
+        launchTimeout: 10000
     }
 };
