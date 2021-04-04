083f1399a9f364fd6c168cffd06b55f9d06419ce
davi-mbatista <machadobatista.davi@gmail.com>
2021-04-04 18:50:13 +0200

    Update

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 import { render, screen } from '@testing-library/preact';
-import App from './App';
+import App from '../App';
 
 beforeAll(() => {
     render(<App />);
